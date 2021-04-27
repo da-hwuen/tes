8955be99cff41907096163073946d91bd83d7334
hjin2017 <hjin2015@gmail>
2021-04-27 15:03:57 +0900

    ddd

---
+++ github-4/src/main/java/com/multi/kdigitalboot2/Github4Application.java
@@ -0,0 +1,13 @@
+package com.multi.kdigitalboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Github4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Github4Application.class, args);
+	}
+
+}
---
+++ github-4/src/test/java/com/multi/kdigitalboot2/Github4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.kdigitalboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Github4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
